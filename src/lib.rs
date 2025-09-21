c181bb0fe26b6ac05730c35f1e326f3c0026029b
Shell Chen <me@sorz.org>
2025-09-22 01:50:30 +1000

    Check password with KV store

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,23 @@
 mod extract;
 
-use axum::{response::IntoResponse, routing::get, Router};
-use axum_extra::{extract::Query, TypedHeader};
+use std::{collections::HashSet, net::IpAddr};
+
+use axum::{extract::State, http::StatusCode, routing::get, Router};
+use axum_extra::{
+    extract::Query,
+    headers::{authorization::Basic, Authorization},
+    TypedHeader,
+};
+use constant_time_eq::constant_time_eq;
+use futures::channel::oneshot;
 use tower_service::Service;
+use wasm_bindgen_futures::spawn_local;
 use worker::{event, Context, Env, HttpRequest};
 
 use crate::extract::{CfConnectingIp, IpAddrs};
 
-fn router() -> Router {
-    Router::new()
-        .route("/", get(root))
-        .route("/update", get(update))
-}
+static KV_HOST_PASSWORD: &str = "ddns_host_password";
+static KV_HOST_PASSWORD_CACHE_SECS: u64 = 600;
 
 #[event(start)]
 fn start() {
@@ -21,10 +27,16 @@ fn start() {
 #[event(fetch)]
 async fn fetch(
     req: HttpRequest,
-    _env: Env,
+    env: Env,
     _ctx: Context,
 ) -> worker::Result<axum::http::Response<axum::body::Body>> {
-    Ok(router().call(req).await?)
+    let resp = Router::<Env>::new()
+        .route("/", get(root))
+        .route("/update", get(update))
+        .with_state(env)
+        .call(req)
+        .await?;
+    Ok(resp)
 }
 
 pub async fn root() -> &'static str {
@@ -32,10 +44,63 @@ pub async fn root() -> &'static str {
 }
 
 async fn update(
+    State(env): State<Env>,
     Query(IpAddrs { ip }): Query<IpAddrs>,
     TypedHeader(CfConnectingIp(client_ip)): TypedHeader<CfConnectingIp>,
-) -> impl IntoResponse {
-    log::debug!("ip {:?}", ip);
+    TypedHeader(Authorization(auth)): TypedHeader<Authorization<Basic>>,
+) -> (StatusCode, String) {
+    // Worker produces non-Send futures while axum requires Send handle
+    // Spawn in local thread as a workaround
+    let (tx, rx) = oneshot::channel();
+    spawn_local(async move {
+        let resp = handle_update_request(env, ip, client_ip, auth).await;
+        tx.send(resp).unwrap();
+    });
+    rx.await.unwrap()
+}
+
+macro_rules! tryit {
+    ($result:expr) => {
+        match $result {
+            Ok(value) => value,
+            Err(err) => {
+                return (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Internal server error: {:?}", err),
+                )
+            }
+        }
+    };
+}
+
+async fn handle_update_request(
+    env: Env,
+    ips: HashSet<IpAddr>,
+    client_ip: IpAddr,
+    auth: Basic,
+) -> (StatusCode, String) {
+    log::debug!("ip {:?}", ips);
     log::debug!("client ip {:?}", client_ip);
-    "ok".into_response()
+
+    // TODO: normalize hostname
+
+    // Check credential
+    let password = tryit!(env.kv(KV_HOST_PASSWORD))
+        .get(auth.username())
+        .cache_ttl(KV_HOST_PASSWORD_CACHE_SECS)
+        .text()
+        .await;
+    match tryit!(password) {
+        Some(pwd) if constant_time_eq(pwd.as_bytes(), auth.password().as_bytes()) => (),
+        _ => {
+            return (
+                StatusCode::UNAUTHORIZED,
+                "hostname/password incorrect".to_string(),
+            )
+        }
+    }
+
+    // TODO: udpate records
+
+    (StatusCode::OK, "ok".to_string())
 }
